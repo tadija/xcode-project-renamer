c6015f473d066bd6e951e7baeb7dcd25b6cf4828
Marko Tadic <marko.tadic@appculture.com>
2016-08-02 16:02:01 +0200

    Initial Commit

---
+++ XcodeProjectRenamer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodeProjectRenamer
+//
+//  Created by Marko Tadic on 8/2/16.
+//  Copyright © 2016 appculture. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
