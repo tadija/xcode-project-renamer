1b89a383ff513f75445e4fc5ffded7e17c558321
Marko Tadic <marko.tadic@appculture.com>
2016-08-03 10:54:16 +0200

    Minor logic and visual improvements

--- XcodeProjectRenamer/main.swift
+++ XcodeProjectRenamer/main.swift
@@ -12,6 +12,19 @@ import Foundation
 
 class XcodeProjectRenamer: NSObject {
     
+    // MARK: - Constants
+    
+    struct Color {
+        static let Black = "\u{001B}[0;30m"
+        static let Red = "\u{001B}[0;31m"
+        static let Green = "\u{001B}[0;32m"
+        static let Yellow = "\u{001B}[0;33m"
+        static let Blue = "\u{001B}[0;34m"
+        static let Magenta = "\u{001B}[0;35m"
+        static let Cyan = "\u{001B}[0;36m"
+        static let White = "\u{001B}[0;37m"
+    }
+    
     // MARK: - Properties
     
     let fileManager = FileManager.default
@@ -24,39 +37,45 @@ class XcodeProjectRenamer: NSObject {
     init(oldName: String, newName: String) {
         self.oldName = oldName
         self.newName = newName
-        
-        print("Rename Xcode Project from [\(oldName)] to [\(newName)]")
-        print("Current Path: \(fileManager.currentDirectoryPath)")
     }
     
     // MARK: - API
     
     func run() {
+        print("\n\(Color.Green)------------------------------------------")
+        print("\(Color.Green)Rename Xcode Project from [\(oldName)] to [\(newName)]")
+        print("\(Color.Green)Current Path: \(fileManager.currentDirectoryPath)")
+        print("\(Color.Green)------------------------------------------")
+        
         handleUnitTests()
         handleUITests()
         handleMainTarget()
         handleProjectFile()
+        
+        print("\n\(Color.Green)------------------------------------------")
+        print("\(Color.Green)Xcode Project Rename Finished!")
+        print("\(Color.Green)------------------------------------------\n")
     }
     
     // MARK: - Helpers
     
     private func handleUnitTests() {
-        print("Unit Tests")
+        print("\n\(Color.Magenta)--- [Unit Tests]")
         enumerateCurrentPath(appendingPathComponent: "\(oldName) Tests")
     }
     
     private func handleUITests() {
-        print("UI Tests")
+        print("\n\(Color.Magenta)--- [UI Tests]")
         enumerateCurrentPath(appendingPathComponent: "\(oldName) UITests")
     }
     
     private func handleMainTarget() {
-        print("Main Target")
+        print("\n\(Color.Magenta)--- [Main Target]")
         enumerateCurrentPath(appendingPathComponent: oldName)
     }
     
     private func handleProjectFile() {
-        print("Project File")
+        print("\n\(Color.Magenta)--- [Project File]")
         enumerateCurrentPath(appendingPathComponent: "\(oldName).xcodeproj")
     }
     
@@ -69,15 +88,22 @@ class XcodeProjectRenamer: NSObject {
     }
     
     private func enumerateDirectory(atPath path: String) {
-        print("\nDIRECTORY: \(path)")
+        print("\n\(Color.Yellow)- DIRECTORY: \(path)")
         
         let enumerator = fileManager.enumerator(atPath: path)
         
         while let element = enumerator?.nextObject() as? String {
             if !shouldSkip(element) {
-                let filePath = path.appending(element)
-                updateContentsOfFile(atPath: filePath)
-                renameItem(atPath: filePath)
+                let itemPath = path.appending(element)
+                
+                var isDir: ObjCBool = false
+                if fileManager.fileExists(atPath: itemPath, isDirectory: &isDir) {
+                    if !isDir.boolValue {
+                        updateContentsOfFile(atPath: itemPath)
+                    }
+                }
+
+                renameItem(atPath: itemPath)
             }
         }
     }
@@ -85,10 +111,9 @@ class XcodeProjectRenamer: NSObject {
     private func shouldSkip(_ element: String) -> Bool {
         guard !element.contains(".DS_Store") else { return true }
         
-        let ext = URL(fileURLWithPath: element).pathExtension
-        
-        switch ext {
-        case "plist", "xcassets", "appiconset", "json", "png", "lproj":
+        let fileExtension = URL(fileURLWithPath: element).pathExtension
+        switch fileExtension {
+        case "appiconset", "json", "png", "xcuserstate":
             return true
         default:
             return false
@@ -96,41 +121,41 @@ class XcodeProjectRenamer: NSObject {
     }
     
     private func updateContentsOfFile(atPath path: String) {
-        print("\nUPDATE ITEM: \(path)")
+        print("\n\(Color.White)UPDATE ITEM: \(path)")
         
         do {
             let oldContent = try String(contentsOfFile: path, encoding: .utf8)
             if oldContent.contains(oldName) {
-                print("UPDATE: \(path)")
+                print("\(Color.Blue)UPDATE: \(path)")
                 let newContent = oldContent.replacingOccurrences(of: oldName, with: newName)
                 try newContent.write(toFile: path, atomically: true, encoding: .utf8)
             } else {
-                print("SKIP: \(path)")
+                print("\(Color.White)SKIP: \(path)")
             }
             
         } catch {
-            print("\(error)\n")
+            print("\(Color.Red)\(error)\n")
         }
     }
     
     private func renameItem(atPath path: String) {
-        print("\nRENAME ITEM: \(path)")
+        print("\n\(Color.White)RENAME ITEM: \(path)")
         
         do {
             let oldItemName = URL(fileURLWithPath: path).lastPathComponent
             if oldItemName.contains(oldName) {
                 let newItemName = oldItemName.replacingOccurrences(of: oldName, with: newName)
-                print("RENAME: \(oldItemName) -> \(newItemName)")
+                print("\(Color.Blue)RENAME: \(oldItemName) -> \(newItemName)")
                 
                 let directoryURL = URL(fileURLWithPath: path).deletingLastPathComponent()
                 let newPath = directoryURL.appendingPathComponent(newItemName).path
                 
                 try fileManager.moveItem(atPath: path, toPath: newPath)
             } else {
-                print("SKIP: \(path)")
+                print("\(Color.White)SKIP: \(path)")
             }
         } catch {
-            print(error)
+            print("\(Color.Red)\(error)")
         }
     }
     
